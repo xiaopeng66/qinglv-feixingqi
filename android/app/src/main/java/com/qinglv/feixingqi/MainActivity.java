f6fb2425fafbd7ea7e9b2cb7d46130713af8243b
xiaopeng66 <xiaopeng66@users.noreply.github.com>
2026-08-21 01:40:28 +0800

    Add branded launch transition

--- android/app/src/main/java/com/qinglv/feixingqi/MainActivity.java
+++ android/app/src/main/java/com/qinglv/feixingqi/MainActivity.java
@@ -1,5 +1,13 @@
 package com.qinglv.feixingqi;
 
+import android.os.Bundle;
+import androidx.core.splashscreen.SplashScreen;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.installSplashScreen(this);
+        super.onCreate(savedInstanceState);
+    }
+}
